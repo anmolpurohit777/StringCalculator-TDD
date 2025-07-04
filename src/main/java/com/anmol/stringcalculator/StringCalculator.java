3625e1c26bdef32502ac56ce66c7fe737505612e
anmolpurohit777 <anmol.purohit04@gmail.com>
2025-07-04 21:45:16 +0530

    [Step-3] Passes Test: Support newline characters in addition to commas as delimiter

--- src/main/java/com/anmol/stringcalculator/StringCalculator.java
+++ src/main/java/com/anmol/stringcalculator/StringCalculator.java
@@ -8,7 +8,7 @@ public class StringCalculator {
 
         if (isEmpty(input)) return 0;
         if (isSingleNumber(input)) return parseSingleNumber(input);
-        if (isCommaOnly(input)) return sumDelimited(input, ",");
+        if (containsCommaOrNewline(input)) return sumDelimited(input, "[,\n]");
 
         return -1;
     }
@@ -21,17 +21,17 @@ public class StringCalculator {
         return !input.contains(",") && !input.contains("\n");
     }
 
-    private boolean isCommaOnly(String input) {
-        return input.contains(",") && !input.contains("\n");
+    private boolean containsCommaOrNewline(String input) {
+        return input.contains(",") || input.contains("\n");
     }
 
     private int parseSingleNumber(String input) {
         return Integer.parseInt(input.trim());
     }
 
-    private int sumDelimited(String input, String delimiter) {
+    private int sumDelimited(String input, String delimiterRegex) {
 
-        String[] tokens = input.split(Pattern.quote(delimiter));
+        String[] tokens = input.split(delimiterRegex);
         int sum = 0;
 
         for (String token : tokens) {
